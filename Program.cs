5d335807a2d6ba321a01aa6b9e07862e58f9ad9f
supchyan <mikhailsupov@yandex.ru>
2025-02-17 13:11:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using IWshRuntimeLibrary;
+
+namespace LolibarAutorun;
+class StartUpScript {
+    public static void CreateShortcut(string targetPath, string shortcutPath)
+    {
+        WshShell shell = new();
+        IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath);
+
+        shortcut.TargetPath = targetPath;
+        shortcut.IconLocation = targetPath;
+
+        shortcut.Save();
+    }
+    public static void Main(string[] args) {
+        var orig_path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var path = orig_path?.Replace(orig_path.Split('\\').Last(), "");
+        
+        if (Path.Exists("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\lolibar.lnk"))
+        {
+            System.IO.File.Delete("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\lolibar.lnk");
+        }
+        else
+        {
+            try
+            {
+                CreateShortcut(
+                    $"{path}lolibar.exe",
+                    "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\lolibar.lnk"
+                );
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(@"A problem to create Lolibar's shortcut in: C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup...");
+                Console.WriteLine($"Execution path: {orig_path}.");
+                Console.WriteLine($"Expected lolibar.exe's path: {path}lolibar.exe.");
+                Console.WriteLine();
+                Console.WriteLine(e);
+                Console.ReadKey();
+            }
+        }
+    }
+}
